69f82c9685377244e827994e3ac31643c4219ad5
mashoo <mashio.tech@gmail.com>
2020-07-15 13:32:29 +0900

    rubocopでコードをチェック

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module MyWebApps
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     Rails.application.config.i18n.default_locale = :ja
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
     config.hosts << 'ja.lvh.me'
     config.hosts << 'en.lvh.me'
   end
