4c3b3d5da8d179a187a77b3b0d13ec81643488a3
mashoo <mashio.tech@gmail.com>
2020-08-11 01:23:04 +0900

    ajaxを使ってユーザーフォローできるように変更

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,14 +3,12 @@
 class RelationshipsController < ApplicationController
   before_action :authenticate_user!
   def create
-    user = User.find(params[:user_id])
-    current_user.follow(user)
-    redirect_to user_path(user)
+    @user = User.find(params[:user_id])
+    current_user.follow(@user)
   end
 
   def destroy
-    user = User.find(params[:user_id])
-    current_user.unfollow(user)
-    redirect_to user_path(params[:user_id])
+    @user = User.find(params[:user_id])
+    current_user.unfollow(@user)
   end
 end
