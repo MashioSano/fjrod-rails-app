fc80579dcc785b4bb8441f7cd5f27e0962b8e88c
mashoo <mashio.tech@gmail.com>
2021-01-30 13:43:30 +0900

    ユーザーモデルのOmniauth認証のテストを書いた

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe User, type: :model do
         expect { @steave.follow(@tony) }.to change(@steave.active_relationships, :count).by(0)
       end
       it 'followed_by?メソッドがtrueを返すこと' do
-        expect(@tony.followed_by?(@steave)).to be true
+        expect (@tony.followed_by?(@steave)).to be true
       end
     end
     context 'TonyがSteaveをフォローしていないとき' do
@@ -30,11 +30,18 @@ RSpec.describe User, type: :model do
   describe 'omniauth認証によるユーザー登録' do
     context 'uidとproviderが同じ値のユーザーが存在しないとき' do
       it 'ユーザーが作成されること' do
+        auth = OmniAuth.config.add_mock(:github, info: { email: 'thor@example.com', nickname: 'thor' })
+        expect { User.from_omniauth(auth) }.to change(User, :count).by(1)
       end
     end
 
     context 'uidとproviderが同じ値のユーザーがすでに存在するとき' do
+      before do
+        User.create(name: 'thor', email: 'thor@example.com', uid: '123456', provider: 'github', password: 'password', password_confirmation: 'password')
+      end
       it 'ユーザーが作成されないこと' do
+        auth = OmniAuth.config.add_mock(:github, info: { email: 'thor@example.com', nickname: 'thor' }, uid: '123456')
+        expect { User.from_omniauth(auth) }.to change(User, :count).by(0)
       end
     end
   end
