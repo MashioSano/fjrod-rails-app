0a851eac0dfb6b1a2500d304418eaed295833bab
mashoo <mashio.tech@gmail.com>
2021-01-31 00:31:53 +0900

    ユーザーの登録、編集、削除のテストを書いた

---
+++ spec/system/users_registrations_test.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe "Users Registrations", type: :system do
+  let(:tony) { FactoryBot.create(:tony_stark) }
+
+  scenario 'create user' do
+    visit root_path
+    click_link 'アカウント登録'
+    fill_in '名前', with: 'PeterParker'
+    fill_in 'Eメール', with: 'peter@example.com'
+    fill_in 'パスワード', with: 'password'
+    fill_in 'パスワード（確認用）', with: 'password'
+    expect { click_button 'アカウント登録' }.to change { User.count }.by(1) 
+    assert_text 'アカウント登録が完了しました。'
+  end
+
+  scenario 'update user' do
+    login_user(tony.email, tony.password)
+    visit edit_user_registration_path
+    fill_in '名前', with: 'MorganStark'
+    attach_file('db/seeds/images/user_default.jpeg') do
+      find('input[type="file"]').click
+    end
+    click_button '更新'
+    assert_text 'アカウント情報を変更しました。'
+  end
+
+  scenario 'delete user', js: true do
+    login_user(tony.email, tony.password)
+    visit edit_user_registration_path
+    expect {
+      accept_confirm do
+        click_button 'アカウント削除'
+      end
+      # 以下の行がないとダイアログのOKボタンを押した直後にUserのカウントを評価するので、User.coutnはh
+      # 減ってないとエラーが出る assert_textをブロックに入れることで評価を遅らせる
+      assert_text 'アカウントを削除しました。またのご利用をお待ちしております。'
+    }.to change { User.count }.by(1)
+  end
+end
