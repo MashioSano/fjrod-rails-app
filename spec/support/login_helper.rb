30c88992a0a788e96f0562de22b92d2ff2c5149f
mashoo <mashio.tech@gmail.com>
2021-01-30 23:13:47 +0900

    withinを使ってformを絞りこんだ

--- spec/support/login_helper.rb
+++ spec/support/login_helper.rb
@@ -1,8 +1,10 @@
 module LoginHelper
   def login_user(email, password)
     visit new_user_session_path
-    fill_in 'Eメール', with: email
-    fill_in 'パスワード', with: password
-    click_button 'ログイン'
+    within 'form[name=sign_in]' do
+      fill_in 'Eメール', with: email
+      fill_in 'パスワード', with: password
+      click_button 'ログイン'
+    end
   end
 end
