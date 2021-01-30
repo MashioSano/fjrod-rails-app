75d299a67407f459161397554b4993a00d782173
mashoo <mashio.tech@gmail.com>
2021-01-30 19:32:49 +0900

    Capybaraのドライバーの設定を変更

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,16 +1,9 @@
 RSpec.configure do |config|
-  # config.before(:each, type: :system) do
-  #   driven_by :rack_test
-  #   p "rackで実行しました"
-  # end
-
-  config.before(:each, type: :system, js: true) do
-    if ENV['HEADED']
+  config.before(:each, type: :system) do |example|
+    if ENV['HEADED'] || example.metadata[:js]
       driven_by :selenium, using: :chrome
-      p "ブラウザを表示しました"
     else
       driven_by :selenium, using: :headless_chrome
-      p "ヘッドレスで実行しました"
     end
   end
 end
