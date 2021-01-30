278d0f5b004767025612559170b7c7fe53d60f59
mashoo <mashio.tech@gmail.com>
2021-01-30 16:24:38 +0900

    models/user_spec.rbをリファクタ
    インスタンス変数、ローカル変数をletに変換
    changeメソッドにはブロックを渡すようにした

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,44 +4,39 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'ユーザーフォロー' do
-    before do
-      @tony = FactoryBot.create(:tony_stark)
-      @steave = FactoryBot.create(:steave_rogers)
-      @relationship = Relationship.create(follower: @tony, following: @steave)
-    end
+    let(:tony) { FactoryBot.create(:tony_stark) }
+    let(:steave) { FactoryBot.create(:steave_rogers) }
+    let!(:relationship) { Relationship.create(follower: tony, following: steave) }
     context 'SteaveがTonyをフォローしているとき' do
       it 'フォローを解除できること' do
-        expect { @steave.unfollow(@tony) }.to change(@steave.active_relationships, :count).by(-1)
+        expect { steave.unfollow(tony) }.to change { steave.active_relationships.count }.by(-1)
       end
       it '1回しかフォローできないこと' do
-        expect { @steave.follow(@tony) }.to change(@steave.active_relationships, :count).by(0)
+        expect { steave.follow(tony) }.to change { steave.active_relationships.count }.by(0)
       end
       it 'followed_by?メソッドがtrueを返すこと' do
-        expect (@tony.followed_by?(@steave)).to be true
+        expect(tony.followed_by?(steave)).to be true
       end
     end
     context 'TonyがSteaveをフォローしていないとき' do
       it 'フォローできること' do
-        expect { @tony.follow(@steave) }.to change(@tony.active_relationships, :count).by(1)
+        expect { tony.follow(steave) }.to change { tony.active_relationships.count }.by(1)
       end
     end
   end
 
   describe 'omniauth認証によるユーザー登録' do
+    let(:auth) { OmniAuth.config.add_mock(:github, info: { email: 'thor@example.com', nickname: 'thor' }, uid: '123456') }
     context 'uidとproviderが同じ値のユーザーが存在しないとき' do
       it 'ユーザーが作成されること' do
-        auth = OmniAuth.config.add_mock(:github, info: { email: 'thor@example.com', nickname: 'thor' }, uid: '987654')
-        expect { User.from_omniauth(auth) }.to change(User, :count).by(1)
+        expect { User.from_omniauth(auth) }.to change { User.count }.by(1)
       end
     end
 
     context 'uidとproviderが同じ値のユーザーがすでに存在するとき' do
-      before do
-        User.create(name: 'thor', email: 'thor@example.com', uid: '123456', provider: 'github', password: 'password', password_confirmation: 'password')
-      end
+      let!(:authroized_user) { User.create(name: 'peter', email: 'peter@example.com', uid: '123456', provider: 'github', password: 'password', password_confirmation: 'password') }
       it 'ユーザーが作成されないこと' do
-        auth = OmniAuth.config.add_mock(:github, info: { email: 'thor@example.com', nickname: 'thor' }, uid: '123456')
-        expect { User.from_omniauth(auth) }.to change(User, :count).by(0)
+        expect { User.from_omniauth(auth) }.to change { User.count }.by(0)
       end
     end
   end
