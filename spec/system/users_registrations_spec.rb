056d415bb1dcb5c252b9a80b11b3ed27a0ddc1d1
mashoo <mashio.tech@gmail.com>
2021-02-07 16:19:29 +0900

    ユーザ登録のSystem Specを修正

--- spec/system/users_registrations_spec.rb
+++ spec/system/users_registrations_spec.rb
@@ -3,38 +3,37 @@
 require 'rails_helper'
 
 RSpec.describe 'Users Registrations', type: :system do
-  let(:tony) { FactoryBot.create(:tony_stark) }
+  let!(:tony) { FactoryBot.create(:tony_stark, name: 'Tony Stark', email: 'tonystark@example.com') }
 
-  example 'create user' do
+  example 'アカウントを登録する' do
     visit root_path
     click_link 'アカウント登録'
-    fill_in '名前', with: 'PeterParker'
+    fill_in '名前', with: 'Peter Parker'
     fill_in 'Eメール', with: 'peter@example.com'
     fill_in 'パスワード', with: 'password'
     fill_in 'パスワード（確認用）', with: 'password'
-    expect { click_button 'アカウント登録' }.to change { User.count }.by(1)
+    click_button 'アカウント登録'
     expect(page).to have_text('アカウント登録が完了しました。')
+    expect(page).to have_text('Peter Parker ユーザー情報の編集 | ログアウト')
   end
 
-  example 'update user' do
-    login_user(tony.email, tony.password)
+  example 'ユーザー情報を更新する' do
+    login_user('tonystark@example.com', 'password')
     visit edit_user_registration_path
-    fill_in '名前', with: 'MorganStark'
-    attach_file('db/seeds/images/user_default.jpeg') do
-      find('input[type="file"]').click
-    end
+    fill_in '名前', with: 'Morgan Stark'
+    attach_file('db/seeds/images/user_default.jpeg')
     click_button '更新'
     expect(page).to have_text('アカウント情報を変更しました。')
+    expect(page).to have_text('Morgan Stark ユーザー情報の編集 | ログアウト')
   end
 
-  example 'delete user', js: true do
-    login_user(tony.email, tony.password)
+  example 'アカウントを削除する', js: true do
+    login_user('tonystark@example.com', 'password')
     visit edit_user_registration_path
-    expect do
-      accept_confirm do
-        click_button 'アカウント削除'
-      end
-      expect(page).to have_text('アカウントを削除しました。またのご利用をお待ちしております。')
-    end.to change { User.count }.by(-1)
+    accept_confirm do
+      click_button 'アカウント削除'
+    end
+    expect(page).to have_text('アカウントを削除しました。またのご利用をお待ちしております。')
+    expect(page).to have_text('アカウント登録 | ログイン')
   end
 end
