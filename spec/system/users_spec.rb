8ad67ebc7a039c0f6a409d871d5b3ff6e85d3d1a
mashoo <mashio.tech@gmail.com>
2021-02-07 15:30:49 +0900

    ユーザー一覧・詳細画面のSystemSpecを修正

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -3,17 +3,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :system do
-  let!(:tony) { FactoryBot.create(:tony_stark) }
-  example 'index users' do
+  let!(:tony) { FactoryBot.create(:tony_stark, name: 'Tony Stark') }
+  example 'ユーザーの一覧を表示する' do
     visit root_path
     click_link 'ユーザー一覧'
-    assert_text 'ユーザー一覧'
+    expect(page).to have_text('ユーザー一覧')
     expect(page).to have_text('Tony Stark')
   end
 
-  example 'show user' do
+  example 'ユーザーの詳細を表示する' do
     visit users_path
-    click_link tony.name
+    click_link 'Tony Stark'
+    expect(page).to have_text('ユーザーの詳細')
     expect(page).to have_text('Tony Stark')
   end
 end
