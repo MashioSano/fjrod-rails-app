0af76a01b5025d9e3bc037675cb6936c85caf847
mashoo <mashio.tech@gmail.com>
2021-02-07 16:03:57 +0900

    ユーザーログイン・ログアウトのSystemSpecを修正

--- spec/system/users_sessions_spec.rb
+++ spec/system/users_sessions_spec.rb
@@ -3,20 +3,22 @@
 require 'rails_helper'
 
 RSpec.describe 'Users Sessions', type: :system do
-  let(:tony) { FactoryBot.create(:tony_stark) }
+  let!(:tony) { FactoryBot.create(:tony_stark, name: 'Tony Stark', email: 'tonystark@example.com') }
 
-  example 'create session' do
+  example 'ログインする' do
     visit root_path
     click_link 'ログイン'
-    fill_in 'Eメール', with: tony.email
-    fill_in 'パスワード', with: tony.password
+    fill_in 'Eメール', with: 'tonystark@example.com'
+    fill_in 'パスワード', with: 'password'
     click_button 'ログイン'
     expect(page).to have_text('ログインしました')
+    expect(page).to have_text('Tony Stark ユーザー情報の編集 | ログアウト')
   end
 
-  example 'delete session' do
-    login_user(tony.email, tony.password)
+  example 'ログアウトする' do
+    login_user('tonystark@example.com', 'password')
     click_link 'ログアウト'
     expect(page).to have_text('ログアウトしました')
+    expect(page).to have_text('アカウント登録 | ログイン')
   end
 end
