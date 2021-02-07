7e2da354951bed803e4648fd14161fd6dc9988dc
mashoo <mashio.tech@gmail.com>
2021-02-07 14:43:15 +0900

    RelationshipのSystemSpecを修正

--- spec/system/relationships_spec.rb
+++ spec/system/relationships_spec.rb
@@ -3,24 +3,24 @@
 require 'rails_helper'
 
 RSpec.describe 'Relationships', type: :system, js: true do
-  let(:tony) { FactoryBot.create(:tony_stark) }
-  let(:steve) { FactoryBot.create(:steve_rogers) }
-  let!(:relationship) { Relationship.create(following: tony, follower: steve) }
+  let!(:tony) { FactoryBot.create(:tony_stark, name: 'Tony Stark', email: 'tonystark@example.com') }
+  let!(:steve) { FactoryBot.create(:steve_rogers, name: 'Steve Rogers', email: 'steverogers@example.com') }
+  let!(:relationship) { Relationship.create!(following: tony, follower: steve) }
 
-  scenario 'create relationship' do
-    login_user(steve.email, steve.password)
+  scenario 'フォローする' do
+    login_user('steverogers@example.com', 'password')
     visit users_path
-    click_link tony.name
+    click_link 'Tony Stark'
     expect do
       click_button 'フォロー'
       expect(page).to have_css('.unfollow-btn')
     end.to change { steve.active_relationships.count }.by(1)
   end
 
-  scenario 'delete relationship' do
-    login_user(tony.email, tony.password)
+  scenario 'フォローを外す' do
+    login_user('tonystark@example.com', 'password')
     visit users_path
-    click_link steve.name
+    click_link 'Steve Rogers'
     expect do
       click_button 'フォローを解除'
       expect(page).to have_css('.follow-btn')
