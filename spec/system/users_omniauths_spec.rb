1e3c8453db359d0e71421fefcdadd0f1af032638
mashoo <mashio.tech@gmail.com>
2021-02-07 15:04:00 +0900

    OmniauthでログインするSystemSpecを修正

--- spec/system/users_omniauths_spec.rb
+++ spec/system/users_omniauths_spec.rb
@@ -3,13 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe 'Users Omniauth', type: :system do
-  example 'user create by omniauth' do
+  before do
+    OmniAuth.config.add_mock(:github, info: { email: 'thor@example.com', nickname: 'Thor' })
+  end
+  example 'GitHubのアカウントでログインする' do
     visit root_path
     click_link 'ログイン'
-    OmniAuth.config.add_mock(:github, info: { email: 'thor@example.com', nickname: 'Thor' })
-    expect do
-      click_link 'GitHubでログイン'
-      expect(page).to have_text('Github アカウントによる認証に成功しました。')
-    end.to change { User.count }.by(1)
+    click_link 'GitHubでログイン'
+    expect(page).to have_text('Github アカウントによる認証に成功しました。')
+    expect(page).to have_text('Thor ユーザー情報の編集 | ログアウト')
   end
 end
